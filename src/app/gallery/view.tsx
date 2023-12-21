8ee59169da5aba2135fc409e51875c71511bb7ed
wajidminhas <shanitent667@gmail.com>
2023-12-21 23:33:23 +0500

    favourite items added

--- src/app/gallery/view.tsx
+++ src/app/gallery/view.tsx
@@ -1,20 +1,28 @@
-"use client"
+"use client";
 
-import { CldImage } from 'next-cloudinary';
+import { CldImage } from "next-cloudinary";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
-const View = ({src} : {src: string}) => {
+const View = ({ src }: { src: string }) => {
   return (
-    <div>
-        <CldImage
-        className='rounded-md'
-width="600"
-height="400"
-src={src}
-sizes="100vw"
-alt="Description of my image"
-/>
+    <div className="relative">
+      <CldImage
+        className="rounded-md"
+        width="600"
+        height="400"
+        src={src}
+        sizes="100vw"
+        alt="Description of my image"
+      />
+        {/* Favourite icons added  */}
+      <div className="absolute top-1 right-1">
+      <AiOutlineHeart
+        className="w-6 h-6 text-white
+     hover:text-red-600 duration-300 cursor-pointer"
+      />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default View
+export default View;
